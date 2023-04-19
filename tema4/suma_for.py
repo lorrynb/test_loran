b9031ee51a36274158693fe2f3a4283fb7260f4d
lorrrynb <nbloran@gmail>
2023-04-19 23:47:18 +0300

    new

---
+++ tema4/suma_for.py
@@ -0,0 +1,5 @@
+a=[34,56,78,98,45,100]
+sum_elem=0
+for i in a:
+    sum_elem += i
+print('syma eleventiv spysku',sum_elem)
---
+++ tema4/suma_while.py
@@ -0,0 +1,7 @@
+a=[34,56,78,98,45,100]
+sum_elem=0
+i=len(a)-1
+while i>=0:
+    sum_elem += a[i]
+    i-=1
+print('syma eleventiv spysku',sum_elem)
